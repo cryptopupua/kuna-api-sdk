1d64dfc6a058a4f2cc898ce3304a3266bcfca5ed
God Jah <god@fastfen.club>
2021-08-13 13:42:47 +0300

    added kunacode script

--- kunacode.js
+++ kunacode.js
@@ -3,9 +3,9 @@ const keys = {
     secretKey: 'b0nsAEq6urPpkpGxzwl7dwvJR4dhwf2VpP4mTIyi',
   };
 
-  const kuna = require('./v3')(keys); 
+const kuna = require('./v3')(keys); 
 
-
-  kuna.private.accountInfo()
+   kuna.public.checkKunaCode('5jKoj')
   .then((data) => console.log(data))
-  .catch(err => console.log('Error: ', err));
+  .catch(err => console.log('Error: ', err)); 
+  
