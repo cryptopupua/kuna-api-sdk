81b4bba8437cc790feea603b3932bd278373e774
CodeMasterBoom <boomhacker@cryptopup.space>
2021-08-16 16:49:20 +0300

    process kuna code in argument, or promt if argument absent

--- kunacode.js
+++ kunacode.js
@@ -1,14 +1,31 @@
-const keys = {
+function checkCode(inkunacode) {
+  var kunacodepattern1=inkunacode.slice(0, 5);
+
+  const keys = {
     publicKey: 'u8vHSYIZy9Dst0qu3J1xrttxoHibCS5miLXrXOoS',
     secretKey: 'b0nsAEq6urPpkpGxzwl7dwvJR4dhwf2VpP4mTIyi',
   };
 
-const kuna = require('./v3')(keys); 
+  const kuna = require('./v3')(keys); 
 
-  kuna.public.checkKunaCode('5jKoj')
-  .then((data) => console.log(data))
-  .catch(err => console.log('Error: ', err)); 
+  return  kuna.public.checkKunaCode(kunacodepattern1)
+  .then((data) => data)
+  .catch((err) => err);   
+}
+
+const prompt = require('prompt-sync')();
+
+if (process.argv.length < 3)
+{
+  const kunacode = prompt('Get me kuba code:');
+  console.log(checkCode(kunacode));
+} else {
+  console.log(checkCode(process.argv.slice(2)[0])); 
+}
+
+ 
+
   
-  kuna.private.activateCode('wfD2V-GCnnw-R9V9L-TyphL-JPd3q-zRUpi-Rtd1N-H4SMr-cXjMx-UAH-KCode')
+/*   kuna.private.activateCode('wfD2V-GCnnw-R9V9L-TyphL-JPd3q-zRUpi-Rtd1N-H4SMr-cXjMx-UAH-KCode')
   .then((data) => console.log(data))
-  .catch(err => console.log('Error: ', err)); 
+  .catch(err => console.log('Error: ', err));  */
