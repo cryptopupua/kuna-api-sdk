148665c382145093aaa44ad8b5f2ba0b7881371e
God Jah <god@fastfen.club>
2021-08-19 21:25:50 +0300

    add balance.js

--- balance.js
+++ balance.js
@@ -5,6 +5,6 @@ const keys = {
 
   const kuna = require('./v3')(keys); 
 
-  kuna.private.accountInfo()
+  kuna.private.accountBalance()
   .then((data) => console.log(data))
   .catch(err => console.log('Error: ', err));
