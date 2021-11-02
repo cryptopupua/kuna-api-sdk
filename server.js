74a1a2ca155b15215acbb9b175b6e93911a308cf
godJah <43356590+stgodjah@users.noreply.github.com>
2021-11-02 13:26:41 +0200

    added server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const config = require("config");
+
+const  app = express();
+
+async function start() {
+    try {
+        
+    } catch (e) {
+    };
+};
+
+start();
