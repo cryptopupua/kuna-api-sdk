01ec8f6268f2049754c71ff049f0821ae5f7b403
godJah <43356590+stgodjah@users.noreply.github.com>
2021-11-14 22:50:42 +0200

    done method getbalance

--- routes/account.routes.js
+++ routes/account.routes.js
@@ -1,20 +1,28 @@
 const { Router } = require('express')
 const { check, validationResult } = require('express-validator')
 const router = Router()
+const keys = require('../keys');
+const kuna = require('../v3')(keys); 
 
 ///api/account/getbalance
 router.get(
-    '/getbalance', [
-        check('email', 'Incorrect email').isEmail(),
-        check('passsword', 'Minimal password lenght 6')
-        .isLength( min=6 )
-    ],
+    '/getbalance',
     async(req, res) => {
         try {
-            res.status(201).json({ message: 'Balance here ...' })
+            let rawbalance  = await kuna.private.accountBalance();
+            let balance = [];
+            if (rawbalance instanceof Array) {
+                rawbalance.forEach(function(currentCurrency){
+                  let currentAmount = currentCurrency[currentCurrency.length-1];
+                  if (currentAmount > 0) {
+                    balance.push(`Currency code:${currentCurrency[1]} ammount:${currentAmount}`);
+                  }
+                });
+              } 
+            res.status(201).json({ balance })
 
         } catch (e) {
-            res.status(500).json({ message: 'Something wrong, try later ...' })
+            res.status(500).json({ message: `Error:` + e.message })
         }
     })
 
