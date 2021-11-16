4d9f5872b164225496651cf5fdb581d00f7d8a4f
CodeMasterBoom <boomhacker@cryptopup.site>
2021-11-16 02:37:35 +0200

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 const keys = {
-    publicKey: 'u8vHSYIZy9Dst0qu3J1xrttxoHibCS5miLXrXOoS',
-    secretKey: 'b0nsAEq6urPpkpGxzwl7dwvJR4dhwf2VpP4mTIyi',
+    publicKey: '',
+    secretKey: '',
   };
 
   module.exports = keys;
