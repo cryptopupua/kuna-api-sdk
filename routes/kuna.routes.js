0f5093be95512a3566f508a0d26b6ba2ea0ea843
CodeMasterBoom <boomhacker@cryptopup.site>
2021-11-25 08:13:53 +0200

    jshint configured

--- routes/kuna.routes.js
+++ routes/kuna.routes.js
@@ -1,6 +1,7 @@
-const { Router } = require('express')
-const { check, body, validationResult } = require('express-validator')
-const router = Router()
+/* jshint esversion: 9 */
+const { Router } = require('express');
+const { check, body, validationResult } = require('express-validator');
+const router = Router();
 const keys = require('../keys');
 const kuna = require('../v3')(keys); 
 
@@ -19,43 +20,42 @@ router.get(
                   }
                 });
               } 
-            res.status(200).json({ balance })
+            res.status(200).json({ balance });
         } catch (e) {
-            res.status(500).json({ message: `Error catched: ${e.message}`})
+            res.status(500).json({ message: `Error catched: ${e.message}`});
         }
       }
-)
+);
 
 ///api/kuna/kunacode
 router.get(
-  '/kunacode',
-  [
-    check('code','Minimal kunacode lenght 5')
-    .isLength(min = 5)
-  ],
-  async (req,res) => {
-    try {
-      const errors = validationResult(req)
+    '/kunacode',
+    [
+      check('code','Minimal kunacode lenght 5').isLength({min: 5})
+    ],
+    async (req,res) => {
+      try {
+        const errors = validationResult(req);
 
-      if (!errors.isEmpty()){
+        if (!errors.isEmpty()){
         return res.status(400).json({
           errors: errors.array(),
           message: 'incorrect kunacode data '
-        })
+        });
       }
 
-      const inkunacode = req.query['code']
+      const inkunacode = req.query.code;
       if (inkunacode.length !== 5)  {
-        kuna.public.validateKunaCode(inkunacode)
+        kuna.public.validateKunaCode(inkunacode);
       }
       var kunacodepattern1=inkunacode.slice(0, 5);
-      const result = await kuna.public.checkKunaCode(kunacodepattern1)
-      res.status(200).json({ kunacodeinfo: result})
+      const result = await kuna.public.checkKunaCode(kunacodepattern1);
+      res.status(200).json({ kunacodeinfo: result});
     } catch (e) {
-      res.status(500).json({ message: `Error catched: ${e.message}`})
-    } 
+      res.status(500).json({ message: `Error catched: ${e.message}`});
+    }
   }
-)
+);
 
 ///api/kuna/kunacodeactivate
 router.post(
@@ -63,27 +63,31 @@ router.post(
   check('code','Minimal kunacode lenght 5').isLength({min: 5}),
   async (req,res) => {
     try {
-      console.log(`Body${req.body}`)
-      const errors = validationResult(req)
+      console.log(`Body${req.body}`);
+      const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
         return res.status(400).json({
           errors: errors.array(),
           message: "Incorrect activation data"
         }
-        )
+        );
       }
     
-      const {code} = req.body 
-      kuna.public.validateKunaCode(code)
-      
-      const result = await kuna.private.activateCode(code)
+      const {code} = req.body ;
+      kuna.public.validateKunaCode(code);
+      var kunacodepattern1=code.slice(0, 5);
+      const result = await kuna.public.checkKunaCode(kunacodepattern1);
+      let kunacodeinfo = result;
+      if (kunacodeinfo.redeemed_at === null) {
+        const result = await kuna.private.activateCode(code);
+      }
 
-      res.status(201).json({message: `Kunacode activated: ${result}`})
+      res.status(201).json( {message: result } );
     } catch (e) {
-      res.status(500).json( {message: `Error catched: ${e.message}`} )
+      res.status(500).json( {message: `Error catched: ${e.message}`} );
     }
   }
-)
+);
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const config = require('config')
+const express = require('express');
+const config = require('config');
 
-const app = express()
+const app = express();
 
-app.use(express.json({extended: true}))
-app.use(fn = '/api/kuna', require('./routes/kuna.routes'))
+app.use(express.json({extended: true}));
+app.use(fn = '/api/kuna', require('./routes/kuna.routes'));
 
-const PORT = config.get('port') || 5000
+const PORT = config.get('port') || 5000;
 
 async function start() {
     try { 
-        app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}`));
     } catch (e) {
-        console.log('Server error', e.message)
-        process.exit(code=1)
-    };
-};
+        console.log('Server error', e.message);
+        process.exit(code=1);
+    }
+}
 
 start();
